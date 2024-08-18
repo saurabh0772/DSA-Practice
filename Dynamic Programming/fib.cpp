e02889320cf9de8225bf67c3dd80a72f6873ddcc
Saurabh Kumar <hackeryazama0772@gmail.com>
2024-08-18 10:41:20 +0530

    Both ways

--- Dynamic Programming/fib.cpp
+++ Dynamic Programming/fib.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fib(int n, vector<int> &dp){
-    if(n <= 1) return n;
+// recursive way:
+// int fib(int n, vector<int> &dp){
+//     if(n <= 1) return n;
 
-    if(dp[n] != -1) return dp[n];
+//     if(dp[n] != -1) return dp[n];
 
-    dp[n] = fib(n-1, dp) + fib(n-2, dp);
-}
+//     dp[n] = fib(n-1, dp) + fib(n-2, dp);
+// }
 
 int main(){
 
     int n; cin >> n;
 
     vector<int> dp(n+1, -1);
+    dp[0] = 0; dp[1] = 1;
+    //loops way
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
 
-    cout << fib(n, dp) << endl;
+    cout << dp[n] << endl;
 
 }
